efdd91bddc7eedc337cb9e08a276f79f093afb2c
Michael Li <michael.li@clarifyhealth.com>
2018-10-16 16:54:48 +0800

    tttttt [skip ci]

--- main_process.js
+++ main_process.js
@@ -7,7 +7,7 @@ require('electron-reload')(__dirname);
 let mainWindow;
 
 
-let str = '335';
+let str = '3356';
 app.on('ready', () => {
     mainWindow = new BrowserWindow({width: 800, height: 850});
     mainWindow.loadURL(`file://${__dirname}/index.html`);
